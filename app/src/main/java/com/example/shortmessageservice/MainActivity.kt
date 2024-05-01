e77615bbd41e4b6520e7e7afff9c3ae2d22c2d4d
ivan mendoza <ivan.mendoza@outlook.com>
2024-04-30 21:25:50 -0600

    Turning this into a BGS.

--- app/src/main/java/com/example/shortmessageservice/MainActivity.kt
+++ app/src/main/java/com/example/shortmessageservice/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.shortmessageservice
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Handler
@@ -11,33 +12,33 @@ import androidx.core.app.ActivityCompat
 
 class MainActivity : ComponentActivity() {
 
-    private var webService: WebService = WebService()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        webService.startWebServer()
-
-
         if(ActivityCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this,Array(1){ Manifest.permission.INTERNET},101)
+        }
+
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.FOREGROUND_SERVICE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,Array(1){ Manifest.permission.FOREGROUND_SERVICE},101)
+        }
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.WAKE_LOCK) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,Array(1){ Manifest.permission.WAKE_LOCK},101)
         }
 
         if(ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this,Array(1){ Manifest.permission.SEND_SMS},101)
         }
         else{
+            val serviceIntent = Intent(this, WebService::class.java)
+            startService(serviceIntent)
 
-            showToast("helloMyBaby")
+            showToast("Background Service has been launched")
 
         }
 
 
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        webService.stopWebServer()
     }
 
     private fun showToast(message: String) {
--- app/src/main/java/com/example/shortmessageservice/WebService.kt
+++ app/src/main/java/com/example/shortmessageservice/WebService.kt
@@ -1,93 +1,166 @@
 package com.example.shortmessageservice
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import fi.iki.elonen.NanoHTTPD
 import java.net.SocketException
 
-class WebService  : NanoHTTPD(8080) {
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import android.os.PowerManager
+import androidx.core.app.NotificationCompat
+import fi.iki.elonen.NanoHTTPD.newFixedLengthResponse
 
+class WebService : Service() {
+
+    private var server: NanoHTTPD? = null
     private var messageSender: MessageSender = MessageSender()
+    private lateinit var wakeLock: PowerManager.WakeLock
+    override fun onCreate() {
+        super.onCreate()
 
-    override fun serve(session: IHTTPSession): Response {
-        val timeoutMillis = 10000 // Adjust this value as needed
-      //  session.set
-
-        return when {
-            (session.method == Method.GET) -> handleGetRequest(session)
-            (session.method == Method.POST && session.uri == "/sendMessage") -> handlePostEndpoint1(session)
-            else -> newFixedLengthResponse(Response.Status.NOT_FOUND, NanoHTTPD.MIME_PLAINTEXT, "Not found")
-        }
+        val powerManager = getSystemService(Context.POWER_SERVICE) as PowerManager
+        wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "WebService::WakeLock")
     }
-    private fun handleGetRequest(session: IHTTPSession): Response {
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        createNotificationChannel()
+        val notification = createNotification()
+        startForeground(NOTIFICATION_ID, notification.build())
+
+        // Acquire the wake lock
+        wakeLock.acquire()
+
+        if (server == null) {
+            server = object : NanoHTTPD(8080) {
+                override fun serve(session: IHTTPSession): Response {
+                    val response = when {
+                        (session.method == Method.GET) -> handleGetRequest(session)
+                        (session.method == Method.POST && session.uri == "/sendMessage") -> handlePostEndpoint1(session)
+                        else -> newFixedLengthResponse(Response.Status.NOT_FOUND, NanoHTTPD.MIME_PLAINTEXT, "Not found")
+                    }
+
+                    // Send a notification every time a request is completed
+                    sendNotification("Request Completed", "SMS has been sent")
+
+                    return response
+                }
+            }
+            try {
+                server?.start()
+                println("Web server started")
+            } catch (e: Exception) {
+                println("Error starting web server: ${e.message}")
+            }
+        }
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        server?.stop()
+        println("Web server stopped")
+        // Release the wake lock
+        if (wakeLock.isHeld) {
+            wakeLock.release()
+        }
+        super.onDestroy()
+    }
+
+    private fun sendNotification(title: String, content: String) {
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            notificationIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle(title)
+            .setContentText(content)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .build()
+
+        notificationManager.notify(NOTIFICATION_ID, notification)
+    }
+
+    private fun handleGetRequest(session: NanoHTTPD.IHTTPSession): NanoHTTPD.Response {
         val uri = session.uri
         return when (uri) {
             "/health" -> handleHealthCheck()
-            else -> newFixedLengthResponse(Response.Status.NOT_FOUND, NanoHTTPD.MIME_PLAINTEXT, "Not found")
+            else -> newFixedLengthResponse(NanoHTTPD.Response.Status.NOT_FOUND, NanoHTTPD.MIME_PLAINTEXT, "Not found")
         }
     }
-    private fun handlePostEndpoint1(session: IHTTPSession): Response {
-        try {
 
-            // Read the request body as a string
-            // Read the request body as a string
+    private fun handlePostEndpoint1(session: NanoHTTPD.IHTTPSession): NanoHTTPD.Response {
+        try {
             val phoneNumber = session.parms.get("phoneNumber")
             val message = session.parms.get("message")
 
-            // Check if the request body is empty or null
             if (phoneNumber.isNullOrEmpty()) {
                 return newFixedLengthResponse(
-                    Response.Status.BAD_REQUEST,
+                    NanoHTTPD.Response.Status.BAD_REQUEST,
                     NanoHTTPD.MIME_PLAINTEXT,
                     "Missing phoneNumber Parameter"
                 )
             }
             if (message.isNullOrEmpty()) {
                 return newFixedLengthResponse(
-                    Response.Status.BAD_REQUEST,
+                    NanoHTTPD.Response.Status.BAD_REQUEST,
                     NanoHTTPD.MIME_PLAINTEXT,
                     "Missing message Parameter"
                 )
             }
 
-
-            messageSender.sendSMS(phoneNumber,message)
-            // Respond with a simple message indicating the received parameters
+            messageSender.sendSMS(phoneNumber, message)
             return newFixedLengthResponse("Message Sent")
         } catch (e: SocketException) {
-            println("SocketException WA");
-
-            // Handle SocketException gracefully
             e.printStackTrace()
-            // Return an empty response or appropriate error response
-            return newFixedLengthResponse(Response.Status.INTERNAL_ERROR, NanoHTTPD.MIME_PLAINTEXT, "Socket closed")
+            return newFixedLengthResponse(NanoHTTPD.Response.Status.INTERNAL_ERROR, NanoHTTPD.MIME_PLAINTEXT, "Socket closed")
         } catch (e: Exception) {
-            // Handle other exceptions
             e.printStackTrace()
-            // Return an appropriate error response
-            return newFixedLengthResponse(Response.Status.INTERNAL_ERROR, NanoHTTPD.MIME_PLAINTEXT, "Error handling request")
+            return newFixedLengthResponse(NanoHTTPD.Response.Status.INTERNAL_ERROR, NanoHTTPD.MIME_PLAINTEXT, "Error handling request")
         }
     }
 
-    private fun handleHealthCheck(): Response {
-        // Respond with a simple message indicating that the server is healthy
+    private fun handleHealthCheck(): NanoHTTPD.Response {
         return newFixedLengthResponse("Server is up and running!")
     }
 
-     fun startWebServer() {
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
 
-        try {
-            this@WebService.start()
-
-            println("Web server started")
-        } catch (e: Exception) {
-            println("Error starting web server: ${e.message}")
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                "Foreground Service Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            val manager = getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(channel)
         }
     }
 
-    fun stopWebServer() {
-        this@WebService.stop()
-        println("Web server stopped")
+    private fun createNotification(): NotificationCompat.Builder {
+        return NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Foreground Service")
+            .setContentText("Web service is running")
+            .setSmallIcon(R.drawable.ic_notification)
     }
 
+    companion object {
+        private const val CHANNEL_ID = "WebServiceChannel"
+        private const val NOTIFICATION_ID = 1
 
-
+    }
 }
 
